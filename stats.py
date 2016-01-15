7d8a0d0239aa66c4d0b8ced1c344a20e9a17fc17
Jeremiah Kakes <kakes@wisc.edu>
2016-01-15 13:21:58 -0600

    Adding assert to catch input errors

--- stats.py
+++ stats.py
@@ -1,9 +1,12 @@
 #stats.py
 #This is a comment
+
 def mean(vals):
     """Calculate the arithmetic mean of a list of numbers in vals"""
+    assert type(vals) is list, 'Input format is incorrect'
     total = sum(vals)
     length = len(vals)
     return total/length
 
-    print(mean([2,4]))
+
+print(mean([2, 4]))
