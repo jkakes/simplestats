7403b40c27b0f8dfbbd59631e7c3777ba43a234b
Jeremiah Kakes <kakes@wisc.edu>
2016-01-15 13:59:45 -0600

    changed assert to assert_equal

--- test_stats.py
+++ test_stats.py
@@ -1,15 +1,17 @@
 #test_stats.py
 
 from stats import mean
+from nose.tools import assert_equal
+
 
 def test_mean():
-    assert mean([2,4]) == 3.0, 'Simple mean test'
-test_mean()
+    assert_equal(mean([2,4]),3.0)
+#test_mean()
 
 def test_empty_list():
-    assert mean([]) == 0.0
-test_empty_list()
+    assert_equal(mean([]),0.0)
+#test_empty_list()
 
-def test_float
-	assert mean([3,4]) == 3.5
-test_float()
+def test_float():
+	assert_equal(mean([3,4]),3.5)
+#test_float()
