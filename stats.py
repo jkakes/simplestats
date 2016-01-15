863859261d763c88a3d5b25cf999a8077f30f8b9
Jeremiah Kakes <kakes@wisc.edu>
2016-01-15 13:12:21 -0600

    starting to work on stats f(x)

---
+++ stats.py
@@ -0,0 +1,9 @@
+#stats.py
+#This is a comment
+def mean(vals):
+    """Calculate the arithmetic mean of a list of numbers in vals"""
+    total = sum(vals)
+    length = len(vals)
+    return total/length
+
+    print(mean([2,4]))
