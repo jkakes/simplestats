c363e03c7c2932ac487c1a97bc00fc71a8fdd343
Jeremiah Kakes <kakes@wisc.edu>
2016-01-15 14:24:21 -0600

    adding string 2 float

--- stats.py
+++ stats.py
@@ -4,8 +4,13 @@
 def mean(vals):
     """Calculate the arithmetic mean of a list of numbers in vals"""
     assert type(vals) is list, 'Input format is incorrect'
+    new_list = []
+    length - len(vals)
+
+    for i in = length
+    	new_list.append(float(i))
     total = sum(vals)
-    length = len(vals)
+
     if length == 0:
     	return 0.0
     else:
--- test_stats.py
+++ test_stats.py
@@ -15,3 +15,6 @@ def test_empty_list():
 def test_float():
 	assert_equal(mean([3,4]),3.5)
 #test_float()
+
+def test_str_list_mean():
+	assert_equal(mean(['1','2','3']),2.0)
